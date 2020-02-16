1b1adea9c58be178caae929041ffff9162d52105
Benjamin Maher <bmaher1@bu.edu>
2020-02-16 10:29:01 -0500

    Commented api.py

--- api/api.py
+++ api/api.py
@@ -3,25 +3,27 @@ import pickle
 import flask
 import numpy as np
 import pandas as pd
-from flask_restful import Api
 
 app = flask.Flask(__name__)
-api = Api(app)
-
-todos = {}
 
 app.config['DEBUG'] = True
 @app.route('/', methods=['GET', 'POST'])
+# Routes API to localhost
+
 def post_feature():
     petal_width = flask.request.args.get('petal_width')
     petal_length = flask.request.args.get('petal_length')
+    # Get features for model from URL as queries
+
     X = pd.DataFrame({'petal length (cm)': petal_length, 'petal width (cm)': petal_width}, index=[0])
+    # Formats features as DataFrame for prediction
+
     prediction = model.predict(X)
 
     return np.array2string(prediction)
 
 if __name__ == '__main__':
     model = pickle.load(open('model.pickle', 'rb'))
+    # Deserializes model
+
     app.run(debug=True)
-
-
